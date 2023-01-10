6c4500f6af42bca0ca8c37dc6070d89954b10343
Chandranath Gupta <chandranathgupta1807@gmail.com>
2023-01-10 17:54:31 +0530

    merged file

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -8,6 +8,7 @@ const validator = require("../validator/validator")
 { name: { mandatory, unique, example iith}, fullName: {mandatory, example `Indian Institute of Technology, Hyderabad`},
  logoLink: {mandatory}, isDeleted: {boolean, default: false} }
 */
+<<<<<<< HEAD
 exports.createCollege = async function(req,res){
 
 try {
@@ -73,24 +74,140 @@ exports.getCollegeData =async function(req,res){
 
    let getCollegeName= await  collegeModel.findOne({name:collegeName}).select({name:1,fullName:1,logoLink:1})
  
+=======
+  // <===============================GLOBAL REGEX====================>
+
+
+const isValid = function(value){
+    if(typeof value == "undefined"|| value=="null") return false
+
+    if(typeof value == "string" && value.trim().length ==0) return false
+
+    return true
+}
+
+const isValidName = function (name) {
+    return /^[a-z]*$/
+    .test(name)
+
+}
+const isValidfname = function (fullName) {
+    return /^[a-zA-Z,'.\s]{0,150}$/
+    .test(fullName)
+
+}
+
+  let urlreg = /^https?:\/\/(.+\/)+.+(\.(gif|png|jpg|jpeg|webp|svg|psd|bmp|tif))$/i
+
+ 
+// ==================================post Api =================================================================
+
+  exports.createCollege = async function(req,res){
+
+ try{
+    const data = req.body
+
+    if(Object.keys(data).length==0)
+
+        return res.status(400).send({status: false,message:"pls provide input"})
+    
+    let {name, fullName, logoLink} = data
+
+if(!name){
+     return res.status(400).send({status:false , msg: "name is mandatory" })
+    }
+    if(!fullName){
+        return res.status(400).send({status:false , msg: "fullName is mandatory" })
+       }
+       if(!logoLink){
+       
+        return res.status(400).send({status:false , msg: "logoLink is mandatory" })
+
+       }
+       if(!isValid(name)){
+        return res.status(400).send({status:false , msg: "please input valid name"})
+       }
+       if(!isValidName(name)){
+        return res.status(400).send({status : false, msg : "please provide correct name"})
+       }
+
+const uniqueName = await collegeModel.findOne({name:data.name})
+         if(uniqueName){
+    res.status(400).send({status: false , msg: " name is already exist"})
+         }
+
+   if(!isValid(fullName)){
+return res.status(400).send({status : false, msg : "please provide correct full name"})
+   }
+
+if(!isValidfname(fullName))
+    return res.status(400).send({status:false , msg:"please enter valid full name"})
+
+    
+ if(!urlreg.test(logoLink)){
+  return res.status(400).send({status:false , msg: " please enter valid logoLink"})
+ }
+
+
+    const saveCollege = await collegeModel.create(data)
+    res.status(201).send({status:true , msg:saveCollege})
+    
+    } catch(err) {
+        res.status(500).send({ status: false, msg: err.message });
+    }
+}
+// =================================================================================================================== 
+/*
+
+Returns the college details for the requested college (Expect a query parameter by the name collegeName. This is anabbreviated college name. For example iith)
+Returns the list of all interns who have applied for internship at this college.
+The response structure should look like this
+Testing
+To test these apis create a new collection in Postman named Project 2 Internship
+Each api should have a new request in this collection
+Each request in the collection should be rightly named. Eg Create college, Get college details etc
+Each member of each team should have their tests in running state
+
+*/
+
+
+
+// ========================================== get Api==========================================
+
+
+exports.getCollegeData =async function(req,res){
+ try {
+
+   let collegeName =req.query.collegeName
+
+   if(!collegeName || collegeName.collegeName== "" ){
+    return res.status(400).send({status:false , msg: "provide college name in query"})
+   }
+   let getCollegeName= await  collegeModel.findOne({name:collegeName}).select({name:1,fullName:1,logoLink:1})
+
+>>>>>>> a063796a06c1a9287c7ef8f88cd31c794a12bf44
    console.log(getCollegeName);
 
+if(!getCollegeName){
+    return res.status(404).send({status: false , msg: "no data found"})
+}
    let getInternsData = await interModel.find({collegeId:getCollegeName._id}).select({name:1,email:1,mobile:1})
    
+
    let obj ={}
    obj.name=getCollegeName.name
    obj.fullName = getCollegeName.fullName
    obj.logoLink = getCollegeName.logoLink
-
    obj.interns = getInternsData
 
   return  res.status(200).send({status : true , data : obj})
 }
   catch (error) {
+<<<<<<< HEAD
     res.status(500).send({status : false, msg : error.message})
+=======
+   return res.status(500).send({status : false, msg : error.message})
+
+>>>>>>> a063796a06c1a9287c7ef8f88cd31c794a12bf44
  }
-
-
 }
-
-/** */
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -6,28 +6,48 @@ Return HTTP status 201 on a succesful document creation. Also return the documen
 Return HTTP status 400 for an invalid request with a response body like this
 
 
-
-
-{ name: {mandatory}, email: {mandatory, valid email, unique}, mobile: {mandatory, valid mobile number, unique}, 
-collegeId: {ObjectId, ref to college model, isDeleted: {boolean, default: false}}
-POST /functionup/colleges
+Intern Model
+{ name: {mandatory}, email: {mandatory, valid email, unique}, mobile: {mandatory, valid mobile number, unique},
+ collegeId: {ObjectId, ref to college model, isDeleted: {boolean, default: false}}
 
 */
-//const { isValidObjectId } = require("mongoose");
+const { isValidObjectId } = require("mongoose");
 const collegeModel = require("../models/collegeModel");
-const internModel = require("../models/internModel");
-const validator = require("../validator/validator")
+const internModel = require("../models/internModel")
 
+// <==========================GLOBAL REGEX =====================>
+
+function validateEmail(id) {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(id);
+  }
+
+  function validateName(id) {
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(id);
+  }
+
+  function validateMobile(id) {
+    let regex = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/ ;
+    return regex.test(id)
+  }
 
 exports.createInterns = async function(req,res){
-const data = req.body
-const {name , email , mobile,collegeName } = data
-
 try {
-    if(Object.keys(data).length ==0){
-        return res.status(400)>send({status: false , msg: "please provide Input"})
-    }
-    if(!name){ 
+
+const data = req.body
+const {name , email , mobile, collegeId} = data
+
+if(Object.keys(data).length ==0){
+
+return res.status(400)>send({status: false , msg: "please provide Input"})
+
+}
+const {name , email , mobile, collegeName} = data
+
+
+
+    if(!name || data.name ==""){ 
         return res.status(400).send({status: false , msg: "provide your name"})
     }
     if(!email){ 
@@ -37,49 +57,79 @@ try {
         return res.status(400).send({status: false , msg: "provide your mobile"})
     }
 
-    if(!collegeName){
-        return res.status(400).send({status: false , msg: "please provide collegeName"})
+    const uniqueMobile = await internModel.findOne({email: data.mobile})
+    if (uniqueMobile) {
+        return res.status(400).send({status:true , msg:"This mobile no is already exist"})
+
+    }
+    // if(!collegeId){ 
+    //     return res.status(400).send({status: false , msg: "provide your collegeId"})
+    // }
+    // if(!isValidObjectId(collegeId)) {
+    //     return res.status(400).send({status : false , msg : "please provide valid collegeId"})
+    // }
+    
+    // regex daalna h
+    if(!validateEmail(email)) {
+        return res.status(400).send({status : false, msg : "please provide correct Email"})
     }
 
-   
-    
-    if(!validator.validateEmail(email)) {
-        return res.status(400).send({status : false, msg : "please provide correct Email"})
+    if(!validateMobile(mobile)) {
+        return res.status(400).send({status : false, msg : "Please use correct Mobile Number"})
     }
 
     if(!validator.validateInName(name)) {
         return res.status(400).send({status : false, msg : "please provide correct name " })
     }
 
-    if(!validator.validateMobile(mobile)) {
-        return res.status(400).send({status : false, msg : "Please use correct Mobile Number"})
-    }
-
-    //if(!isValidObjectId(collegeId)) return res.status(400).send({status : false, msg : "CollegeId is not Valid"})
+    
+    
     
     const uniqueEmail = await internModel.findOne({email: data.email})
     
     if(uniqueEmail){
         return res.status(400).send({status: false , msg: "email is already exist"})
     }
-
-    const uniqueMobile = await internModel.findOne({mobile: data.mobile})
-    if (uniqueMobile) {
-        return res.status(400).send({status:true , msg:"This mobile no is already exist"})
-
-    }
     
-    const getCollegeId = await collegeModel.findOne({name : data.collegeName}) 
-    console.log(getCollegeId);
-    data.collegeId = getCollegeId["_id"]
+    
+
+    if(!validateMobile(mobile)) {
+        return res.status(400).send({status : false, msg : "Please use correct Mobile Number"})
+    }
+    // const getCollegeId = await collegeModel.fineOne({name : data.collegeName}) 
+    // data.collegeId = getCollegeId["_id"]
     
      const saveInterns = await internModel.create(data)
+    // saveInterns.isDeleted,
     
     res.status(201).send({status:true,data:saveInterns})
-
+}
     
-    }
  catch (error) {
     return res.status(500).send({status : false , msg : error.message})
 }
+}
+
+/*
+
+## GET /functionup/collegeDetails
+Returns the college details for the requested college (Expect a query parameter by the name collegeName. This is anabbreviated college name. For example iith)
+Returns the list of all interns who have applied for internship at this college.
+The response structure should look like this
+Testing
+To test these apis create a new collection in Postman named Project 2 Internship
+Each api should have a new request in this collection
+Each request in the collection should be rightly named. Eg Create college, Get college details etc
+Each member of each team should have their tests in running state
+
+
+*/
+
+const getIntern = async function(req, res){
+
+    const data = req.query
+
+    const showData  = await  internModel.findOne({...data}).populate('collegeId')
+res.status(200).send({status:true , data:showData})
+
 }
